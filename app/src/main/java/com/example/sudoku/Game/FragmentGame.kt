8c43cb648ce33c3b846e2c5a705482790c5e503a
Szymon Kwiatkowski <niijon6@gmail.com>
2022-06-02 19:24:53 +0200

    Stage commit

--- app/src/main/java/com/example/sudoku/Game/FragmentGame.kt
+++ app/src/main/java/com/example/sudoku/Game/FragmentGame.kt
@@ -6,11 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModelProviders
 import com.example.sudoku.R
 import com.example.sudoku.SudokuBoardView
+import com.example.sudoku.databinding.ActivityMainBinding.inflate
 import com.example.sudoku.databinding.FragmentEntryBinding
 import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.android.synthetic.main.fragment_game.view.*
 
 class FragmentGame : Fragment(), SudokuBoardView.OnTouchListener {
     private lateinit var viewModel: FragmentGameViewModel
@@ -19,11 +22,16 @@ class FragmentGame : Fragment(), SudokuBoardView.OnTouchListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        val viewRoot = LayoutInflater.from(this).inflate(R.layout.fragment_game, container, false)
+//        val binding: ViewDataBinding? = DataBindingUtil.bind(viewRoot)
+
+        val view = inflater.inflate(R.id.sudokuBoardView, container, false)
         val binding: FragmentEntryBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_entry, container, false)
-
-
-        sudokuBoardView.registerListener(this)
+//
+//        val binding: SudokuBoardView = SudokuBoardView.inflate(getLayoutInflater(), view, false)
+//        binding.
+        view.sudokuBoardView.registerListener(this)
         viewModel = ViewModelProviders.of(this).get(FragmentGameViewModel::class.java)
         viewModel.selectedCellLiveData.observe(viewLifecycleOwner) { cell ->
             sudokuBoardView.updateSelectedCellUI(cell.first, cell.second) }
